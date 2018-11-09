c28294908fbc53bfbc56e4083a6fc3fe98623f22
Johann <peniskopfbacken@gmail.com>
2018-11-09 19:21:23 +0100

    asdasd

--- resources/views/home/wellcome.blade.php
+++ resources/views/home/wellcome.blade.php
@@ -2,7 +2,7 @@
 <!--
 	Astral by HTML5 UP
 	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)asd
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 -->
 @php
 @endphp
